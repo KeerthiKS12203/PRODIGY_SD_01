3e92ddcd66b463b161d22e5a6b40ac015695f13e
KeerthiKS12203 <135096446+KeerthiKS12203@users.noreply.github.com>
2023-11-21 17:45:33 +0530

    Update TempRun.java

--- TempRun.java
+++ TempRun.java
@@ -71,9 +71,32 @@ public class TempRun implements ActionListener {
 			  symb = 'K'; 
         }
 		temp=Double.parseDouble(t1.getText());
-		TempConv tc=new TempConv();
-		t2.setText(tc.convert(temp,symb));
+		t2.setText(convert(temp,symb));
 		t2.setVisible(true);
+	}
+
+	public String convert(double t, char s){
+		if(s=='F') {
+			return "\n Celcius: "+FarenheitToCelcius(t)+"°C     Kelvin: "+FarenheitToKelvin(t)+"K";
+		}else if(s=='C') {
+			return "\n Farenheit: "+CelciusToFarenheit(t)+"°F     Kelvin: "+CelciusToKelvin(t)+"K";
+		}else if(s=='K') {
+			return "\n Farenheit: "+KelvinToFarenheit(t)+"°F     Celcius: "+KelvinToCelcius(t)+"°C";
+		}
+		return null;
+	}
+	public double FarenheitToCelcius(double f) {
+		return (f-32)/1.8;
+	}public double CelciusToFarenheit(double c) {
+		return 1.8*c+32;
+	}public double FarenheitToKelvin(double f) {
+		return (f-32)/1.8+273.15;
+	}public double KelvinToFarenheit(double k) {
+		return (k-273.15)*1.8+32;
+	}public double CelciusToKelvin(double c) {
+		return c+273.15;
+	}public double KelvinToCelcius(double k) {
+		return k-273.15;
 	}
 	
 	public static void main(String arg[]) {
